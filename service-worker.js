3844289a7e000591bc658ddde811ef118ee45817
the3kingleo-cmyk <the3kingleo@gmail.com>
2026-04-30 19:53:32 -0700

    feat: add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'promptdeed-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      return cached || fetch(e.request).then((resp) => {
+        const respClone = resp.clone();
+        caches.open(CACHE_NAME).then((c) => { try { c.put(e.request, respClone); } catch (err) {} });
+        return resp;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
